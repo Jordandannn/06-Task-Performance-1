b7259f79b3dbd61460116f04e2a810f531895023
Jordandannn <144678132+Jordandannn@users.noreply.github.com>
2023-11-20 22:55:10 +0800

    Add files via upload

---
+++ BasicCalculator/BasicCalculator/FrmBasicCalcultor.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace BasicCalculator
+{
+    partial class FrmBasicCalcultor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnCompute = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Basic Calculator";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox1.Location = new System.Drawing.Point(12, 36);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(210, 29);
+            this.txtBox1.TabIndex = 1;
+            this.txtBox1.Text = "0";
+            this.txtBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBox2.Location = new System.Drawing.Point(12, 106);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(210, 29);
+            this.txtBox2.TabIndex = 2;
+            this.txtBox2.Text = "0";
+            this.txtBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.comboBox1.Location = new System.Drawing.Point(84, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(59, 29);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.SandyBrown;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 141);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(210, 45);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "Total:\n00000";
+            // 
+            // btnCompute
+            // 
+            this.btnCompute.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnCompute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCompute.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCompute.Location = new System.Drawing.Point(55, 192);
+            this.btnCompute.Name = "btnCompute";
+            this.btnCompute.Size = new System.Drawing.Size(127, 29);
+            this.btnCompute.TabIndex = 6;
+            this.btnCompute.Text = "Compute";
+            this.btnCompute.UseVisualStyleBackColor = false;
+            this.btnCompute.Click += new System.EventHandler(this.btnCompute_Click);
+            // 
+            // FrmBasicCalcultor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PeachPuff;
+            this.ClientSize = new System.Drawing.Size(234, 231);
+            this.Controls.Add(this.btnCompute);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmBasicCalcultor";
+            this.Text = "FrmBasicCalculator";
+            this.Load += new System.EventHandler(this.FrmBasicCalcultor_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Button btnCompute;
+        public System.Windows.Forms.TextBox txtBox1;
+        public System.Windows.Forms.TextBox txtBox2;
+        public System.Windows.Forms.ComboBox comboBox1;
+        public System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ BasicCalculator/BasicCalculator/FrmBasicCalcultor.cs
@@ -0,0 +1,70 @@
+﻿using CalculatorPrivateAssembly;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BasicCalculator
+{
+    public partial class FrmBasicCalcultor : Form
+    {
+        public FrmBasicCalcultor()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmBasicCalcultor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCompute_Click(object sender, EventArgs e)
+        {
+
+            if (txtBox1.Text.Equals("")|| txtBox2.Text.Equals(""))
+            {
+                MessageBox.Show("Please input number!");
+            }
+            else
+            {
+                float num1 = (float)Convert.ToDouble(txtBox1.Text);
+                float num2 = (float)Convert.ToDouble(txtBox2.Text);
+
+                if (comboBox1.Text == "+")
+                {
+                    float total = BasicComputation.Addition(num1, num2);
+                    //string total = total.ToString();
+                    richTextBox1.Text = "Total: /n" + total.ToString();
+                }
+                else if (comboBox1.Text.Equals("-"))
+                {
+                    float total = BasicComputation.Subtraction(num1, num2);
+                    richTextBox1.Text = "Total: " + total.ToString();
+                }
+                else if (comboBox1.Text.Equals("*"))
+                {
+                    float total = BasicComputation.Multiplication(num1, num2);
+                    richTextBox1.Text = "Total: " + total.ToString();
+                }
+                else if (comboBox1.Text.Equals("/"))
+                {
+                    float total = BasicComputation.Division(num1, num2);
+                    richTextBox1.Text = "Total: " + total.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Please Select Operator");
+                }
+            }
+            
+            
+        }
+    }
+}
